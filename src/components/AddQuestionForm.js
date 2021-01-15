8cf9c48285bed3e6cacb1f167b7c94bb4617bc3f
Amit Kahlon <50583120+Amitkahlon@users.noreply.github.com>
2021-01-15 06:49:29 +0200

    Added Tags input

--- src/components/AddQuestionForm.js
+++ src/components/AddQuestionForm.js
@@ -12,18 +12,23 @@ import {
     Icon
 } from 'semantic-ui-react'
 import AnswerForm from "./AnswerForm";
+import QuestionTagInput from './QuestionTagInput';
 
-const questionType = [
+const questionTypeOptions = [
     { key: 1, text: 'Single Answer', value: 1 },
     { key: 2, text: 'Multi Answer', value: 2 },
 ]
 
 
 const AddQuestionForm = () => {
-
     const [answers, setAnswers] = useState([]);
+    const [title, setTitle] = useState("");
+    const [subTitle, setSubTitle] = useState("");
+    const [questionType, setQuestionType] = useState("");
+    const [answersDisplay, setAnswersDisplay] = useState("");
+    const [tags, setTags] = useState([{ id: 'Question', text: 'Question' }, { id: 'Hard', text: 'Hard' }]);
 
-    const AddAnswerForm = () => {
+    const addAnswerForm = () => {
         let newAnswer = { answer: "answer", correct: false }
         setAnswers([...answers, newAnswer]);
     }
@@ -34,38 +39,84 @@ const AddQuestionForm = () => {
         setAnswers(newArray);
     }
 
+    const submitQuestion = () => {
+        const question = {
+            title: title,
+            subTitle: subTitle,
+            questionType: questionType,
+            tags: [...tags],
+            answers: [...answers],
+            answersDisplay: answersDisplay
+        }
+
+        console.log(question);
+    }
+
     return (
         <Form>
             <Form.Field
                 control={TextArea}
                 label='Question Text'
                 placeholder='Enter question text...'
+                value={title}
+                onChange={(e, { value }) => setTitle(value)}
             />
             <Form.Field
                 control={TextArea}
                 label='Question Sub Text'
                 placeholder='Enter question sub text...'
+                value={subTitle}
+                onChange={(e, { value }) => setSubTitle(value)}
             />
             <Form.Dropdown width="10"
+                // name="subject"
+                value={questionType}
+                onChange={(e, { value }) => setQuestionType(value)}
                 control={Dropdown}
-                options={questionType}
+                options={questionTypeOptions}
                 selection
                 placeholder="Question Type"
             />
+
+            <Form.Field>
+                Selected value:
+            </Form.Field>
+            <Form.Field>
+                <Radio
+                    label='Vertical'
+                    name='radioGroup'
+                    value='vertical'
+                    checked={answersDisplay === 'vertical'}
+                    onChange={(e, { value }) => setAnswersDisplay(value)}
+                />
+            </Form.Field>
+            <Form.Field>
+                <Radio
+                    label='Horizontal'
+                    name='radioGroup'
+                    value='horizontal'
+                    checked={answersDisplay === 'horizontal'}
+                    onChange={(e, { value }) => setAnswersDisplay(value)}
+                />
+            </Form.Field>
 
             <Divider />
 
             {
                 answers.map((answerItem, i) =>
-                    <AnswerForm answerItem={answerItem} index={i} key={i} removeAnswer={removeAnswer}/>
+                    <AnswerForm answerItem={answerItem} index={i} key={i} removeAnswer={removeAnswer} />
                 )
             }
 
 
-            <Button floated="right" primary onClick={AddAnswerForm}>
+            <Button floated="right" primary onClick={addAnswerForm}>
                 <Icon name='plus' />
                     Add an answer
-                </Button>
+            </Button>
+
+            <QuestionTagInput tags={tags} setTags={setTags} />
+
+            <Form.Button content='Add Question' primary onClick={submitQuestion} />
         </Form>
     )
 }
