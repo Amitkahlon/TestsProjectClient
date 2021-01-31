dd27ac8fd97088bfdcac86388f347f38dfc59440
Amit Kahlon <50583120+Amitkahlon@users.noreply.github.com>
2021-01-31 18:01:56 +0200

    First version of reports

--- src/components/GenerateTestReportForm.js
+++ src/components/GenerateTestReportForm.js
@@ -1,28 +1,28 @@
 import React from "react";
 import { Button, List, Form, Label, Checkbox } from 'semantic-ui-react'
 
-const GenerateTestReportForm = ({ options }) => {
+const GenerateTestReportForm = ({ form, formDispatch, submitForm }) => {
     return <Form className="centered segment" style={{ width: "80%" }}>
         <List divided>
             <List.Item>
                 <Label horizontal>Select Test: </Label>
-                <Form.Select options={options} placeholder="Test1" />
+                <Form.Select options={form.tests} placeholder="Test1" />
             </List.Item>
             <List.Item>
                 <Label horizontal>From: </Label>
-                <input type="date" name="date"></input>
+                <input type="date" name="date" value={form.fromDate} onChange={(event) => formDispatch({type: "Set_From_Date", payload: {fromDate: event.target.value}}) }></input>
 
                 <Label horizontal>To: </Label>
-                <input type="date" name="date"></input>
+                <input type="date" name="date" value={form.toDate} onChange={(event) => formDispatch({type: "Set_To_Date", payload: {toDate: event.target.value}}) }></input>
             </List.Item>
             <List.Item>
-                <Checkbox inline />
+                <Checkbox inline value={form.anyDate} onChange={(e, { checked }) => formDispatch({type: "Set_Any_Date", payload: {checked }})} />
                 <Label horizontal>Any Date In The Past</Label>
             </List.Item>
 
             <List.Item>
                 <Button floated="left">Back</Button>
-                <Button floated="right">Generate Report</Button>
+                <Button floated="right" onClick={submitForm}>Generate Report</Button>
             </List.Item>
         </List>
     </Form>
--- src/components/TestReport.js
+++ src/components/TestReport.js
@@ -4,26 +4,27 @@ import GenerateTestReportForm from '../components/GenerateTestReportForm';
 import ReportSummary from './ReportSummary';
 import RespondentGradesList from './RespondentGradesList';
 
-const TestReport = ({ reportForm }) => {
+const summary = {
+    testName: "test1",
+    fromDate: "1/1/2020 1",
+    toDate: "1/1/2020",
+    submissionsCount: 3,
+    testId: "s1j3iuasdjhu245",
+    respondentPassed: 2,
+    testType: "???",
+    PassingPrecentage: 33,
+    questionsCount: 15,
+    averageGrade: 60,
+    passingGrade: 55,
+    medianGrade: 63
+}
 
-    const summary = {
-        testName: "test1",
-        fromDate: "1/1/2020",
-        toDate: "1/1/2020",
-        submissionsCount: 3,
-        testId: "s1j3iuasdjhu245",
-        respondentPassed: 2,
-        testType: "???",
-        PassingPrecentage: 33,
-        questionsCount: 15,
-        averageGrade: 60,
-        passingGrade: 55,
-        medianGrade: 63
-    }
+const TestReport = ({ reportForm }) => {
 
     return (
         <Container >
             <ReportSummary summary={summary} />
+
             <Header as="h1">Respondent Grades And Answers </Header>
             <Header as="h4">Click a name form the list to see the respondent test </Header>
 
